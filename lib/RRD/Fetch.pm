98753a457cc8cc99fbdb28e867412cc9847a2e0d
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2025-10-10 13:32:52 -0500

    add the initial files

---
+++ lib/RRD/Fetch.pm
@@ -0,0 +1,105 @@
+package RRD::Fetch;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+RRD::Fetch - The great new RRD::Fetch!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use RRD::Fetch;
+
+    my $foo = RRD::Fetch->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rrd-fetch at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=RRD-Fetch>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc RRD::Fetch
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=RRD-Fetch>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/RRD-Fetch>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/RRD-Fetch>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2025 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 2, June 1991
+
+
+=cut
+
+1; # End of RRD::Fetch
