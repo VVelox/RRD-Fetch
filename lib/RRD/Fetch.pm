3110132ad2ede0974a1e00e3a6677c794e6fd9da
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2025-11-30 20:31:53 -0600

    more misc work

--- lib/RRD/Fetch.pm
+++ lib/RRD/Fetch.pm
@@ -580,12 +580,21 @@ sub daily_stats {
 					push( @values, $current_value );
 				}
 			}
-			$to_return->{'max'}{$current_day}{$column}    = sprintf( '%.12f', max(@values) );
-			$to_return->{'min'}{$current_day}{$column}    = sprintf( '%.12f', min(@values) );
-			$to_return->{'sum'}{$current_day}{$column}    = sprintf( '%.12f', sum(@values) );
-			$to_return->{'mean'}{$current_day}{$column}   = sprintf( '%.12f', mean(@values) );
-			$to_return->{'mode'}{$current_day}{$column}   = sprintf( '%.12f', mode(@values) );
-			$to_return->{'median'}{$current_day}{$column} = sprintf( '%.12f', median(@values) );
+			if (defined($values[0])){
+				$to_return->{'max'}{$current_day}{$column}    = sprintf( '%.12f', max(@values) );
+				$to_return->{'min'}{$current_day}{$column}    = sprintf( '%.12f', min(@values) );
+				$to_return->{'sum'}{$current_day}{$column}    = sprintf( '%.12f', sum(@values) );
+				$to_return->{'mean'}{$current_day}{$column}   = sprintf( '%.12f', mean(@values) );
+				$to_return->{'mode'}{$current_day}{$column}   = sprintf( '%.12f', mode(@values) );
+				$to_return->{'median'}{$current_day}{$column} = sprintf( '%.12f', median(@values) );
+			}else{
+				$to_return->{'max'}{$current_day}{$column}    = 0;
+				$to_return->{'min'}{$current_day}{$column}    = 0;
+				$to_return->{'sum'}{$current_day}{$column}    = 0;
+				$to_return->{'mean'}{$current_day}{$column}   = 0;
+				$to_return->{'mode'}{$current_day}{$column}   = 0;
+				$to_return->{'median'}{$current_day}{$column} = 0;
+			}
 		} ## end foreach my $column ( @{ $to_return->{'columns'}...})
 
 		$t += 86400;
--- lib/RRD/Fetch/Helper/librenms_logsize_daily_stats.pm
+++ lib/RRD/Fetch/Helper/librenms_logsize_daily_stats.pm
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 use String::ShellQuote qw( shell_quote );
 use JSON               qw( decode_json );
+use RRD::Fetch;
 
 =head1 NAME
 
@@ -21,7 +22,17 @@ our $VERSION = '0.0.1';
 
 =head1 SYNOPSIS
 
-=head2 GENERAL FLAGS
+=head2 BASIC FLAGS
+
+=head2 -s <start>
+
+Start time to use.
+
+=head2 -f <days>
+
+How many days to fetch info for.
+
+=head2 SELECTION FLAGS
 
 =head2 --dev <dev spec>
 
@@ -75,7 +86,7 @@ Path to the RRD dir for LibreNMS.
 
 '%%%ldir%%%' will be replace with the path for the LibreNMS dir.
 
-default: %%%ldir%%%/rrd
+If not defined, 'lnms config:get rrd_dir' is used to fetch it.
 
 =head2 --user <user>
 
@@ -88,6 +99,14 @@ default: librenms
 sub action {
 	my ( $self, %opts ) = @_;
 
+	if ( !defined( $opts{'opts'}{'s'} ) ) {
+		die('-s is undef... this should be used for specify the start time');
+	}
+	
+	if ( !defined( $opts{'opts'}{'f'} ) ) {
+		$opts{'opts'}{'f'} = '7';
+	}
+	
 	if ( !defined( $opts{'opts'}{'user'} ) ) {
 		$opts{'opts'}{'user'} = 'librenms';
 	}
@@ -116,14 +135,19 @@ sub action {
 		die( '"' . $lnms . '" is not executable' );
 	}
 
-	if ( !defined( $opts{'opts'}{'rdir'} ) ) {
-		$opts{'opts'}{'rdir'} = '%%%ldir%%%/rrd';
-	}
-	$opts{'opts'}{'rdir'} =~ s/\%\%\%ldir\%\%\%/$opts{'opts'}{'ldir'}/g;
-
 	my $command = $opts{'opts'}{'command'};
 	$command =~ s/\%\%\%user\%\%\%/$opts{'opts'}{'user'}/g;
 	$command = $command . ' ' . $lnms;
+	
+	if ( !defined( $opts{'opts'}{'rdir'} ) ) {
+		$opts{'opts'}{'rdir'} = `$command config:get rrd_dir`;
+		if ($? != 0){
+			die( '"' . $command . ' config:get rrd_dir" exited non-zero with "' . $opts{'opts'}{'rdir'} . '"' );
+		}
+		chomp($opts{'opts'}{'rdir'});
+	}else{
+		$opts{'opts'}{'rdir'} =~ s/\%\%\%ldir\%\%\%/$opts{'opts'}{'ldir'}/g;
+	}
 
 	my $report_devices_command = $command . ' report:devices -o json -r applications';
 	if ( defined( $opts{'opts'}{'dev'} ) ) {
@@ -216,6 +240,38 @@ sub action {
 		} ## end if ($process_dev)
 	} ## end foreach my $device_raw (@report_devices_output_split)
 
+	my $to_return={
+		'dev-sets'=>[],
+			'data'=>{},
+	};
+
+	my %dates;
+	my @device_keys =keys(%{ $devices });
+	foreach my $device (@device_keys){
+		my $base_dir = $opts{'opts'}{'rdir'}.'/'.$device;
+		foreach my $set (@{ $devices->{$device}{'sets'} }){
+			my $rrd = $base_dir.'/app-logsize-'.$devices->{$device}{'app_id'}.'-'.$set.'.rrd';
+			my $rrd_fetch = RRD::Fetch->new('CF'=>'MAX', 'rrd_file' => $rrd);
+			my $daily_stats = $rrd_fetch->daily_stats('start' => $opts{'opts'}{'s'}, 'for' => $opts{'opts'}{'f'});
+
+			my $devset=$device.' '.$set;
+
+			push(@{ $to_return->{'dev-sets'} }, $devset);
+			
+			foreach my $date (@{ $daily_stats->{'dates'} }){
+				$dates{$date}=1;
+				if (!defined($to_return->{'data'}{$date})){
+					$to_return->{'data'}{$date}={};
+				};
+
+				$to_return->{'data'}{$date}{$devset}=$daily_stats->{'max'}{$date}{'max_size'};
+			}
+		}
+	}
+	my @dates_keys=sort(keys(%dates));
+	$to_return->{'dates'}=\@dates_keys;
+
+	return $to_return;
 } ## end sub action
 
 sub opts_data {
@@ -229,6 +285,8 @@ dr=s
 sri
 sr=s
 dev=s
+s=s
+f=s
 ';
 } ## end sub opts_data
 
